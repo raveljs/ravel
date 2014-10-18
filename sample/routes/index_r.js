73349d5fa672f8ef89fdfe856f2b96d726ae2630
Sean McIntyre <s.mcintyre@xverba.ca>
2014-10-18 00:25:20 -0400

    Separated sample project and mysql provider into distinct repositories.

--- sample/routes/index_r.js
+++
@@ -1,9 +0,0 @@
-module.exports = function($E, $L, $RouteBuilder) {
-	$RouteBuilder.public().add('/login', function(req, res) {
-		res.render('login', {});
-	});
-
-	$RouteBuilder.private().add('/', function(req, res) {
-		res.render('app', {});
-	});
-}
