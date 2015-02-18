cbe1956e26d8ca47fbdae09552f83c768f1beea0
Sean McIntyre <s.mcintyre@xverba.ca>
2015-02-17 23:17:05 -0500

    Added tests for auth/csrf

--- test/auth/test-authorize-request.js
+++ test/auth/test-authorize-request.js
@@ -35,7 +35,8 @@ describe('util/authorize_request', function() {
   afterEach(function(done) {
     Ravel = undefined;
     authorizeRequest = undefined;
-    authorizeTokenStub = undefined;
+    authorizeTokenStub = undefined;    
+    tokenToProfile = undefined;
     mockery.disable();
     done();
   });
