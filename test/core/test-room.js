ef612d98992c1abfd1d52f28600695a58750f6ee
Sean McIntyre <s.mcintyre@xverba.ca>
2015-02-23 19:24:32 -0500

    Added tests for core/room
    Also implemented shorthand for defining 'public' rooms which do not have authorization functions.

--- test/core/test-room.js
+++ test/core/test-room.js
@@ -1,1 +1,80 @@
-require('../../lib-cov/core/room');
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+chai.use(require('chai-things'));
+
+var Ravel, rooms;
+
+describe('Ravel', function() {
+  beforeEach(function(done) {
+    rooms = {};
+    Ravel = new require('../../lib-cov/ravel')();
+    Ravel.Log.setLevel(Ravel.Log.NONE);
+    Ravel.kvstore = {}; //mock Ravel.kvstore, since we're not actually starting Ravel.
+    require('../../lib-cov/core/room')(Ravel, rooms);
+    done();
+  });
+
+  afterEach(function(done) {
+    rooms = undefined;
+    Ravel = undefined;
+    done();
+  });
+
+  describe('#room()', function() {
+    it('should throw Ravel.ApplicationError.DuplicateEntry if a room with a given pattern has already been registered', function(done) {
+      rooms['/test'] = {};
+      try {
+        Ravel.room('/test');
+        done(new Error('Duplicate room patterns should result in a Ravel.ApplicationError.DuplicateEntry'));
+      } catch (err) {
+        expect(err).to.be.instanceof(Ravel.ApplicationError.DuplicateEntry);
+        done();
+      }
+    });
+
+    it('should throw Ravel.ApplicationError.IllegalValue if the supplied authorization function is not a function', function(done) {
+      try {
+        Ravel.room('/test', {});
+        done(new Error('Non-function authorizationFunctions should result in a Ravel.ApplicationError.IllegalValue'));
+      } catch (err) {
+        expect(err).to.be.instanceof(Ravel.ApplicationError.IllegalValue);
+        done();
+      }
+    });
+
+    it('should permit clients to register rooms which support path parameters and authorization functions', function(done) {
+      var authorizationFunction = function(userId, callback) {
+        callback(null, true);
+      };
+      Ravel.room('/test/:testId/entity/:entityId', authorizationFunction);
+      expect(rooms).have.property('/test/:testId/entity/:entityId').that.deep.equals({
+        name: '/test/:testId/entity/:entityId',
+        params: ['testId', 'entityId'],
+        regex: new RegExp('/test/(\\w+)/entity/(\\w+)'),
+        authorize: authorizationFunction
+      });
+      var match = '/test/1/entity/2'.match(rooms['/test/:testId/entity/:entityId'].regex);
+      expect(match[0]).to.equal('/test/1/entity/2');
+      expect(match[1]).to.equal('1');
+      expect(match[2]).to.equal('2');
+      done();
+    });
+
+    it('should permit clients to register public rooms without authorization functions', function(done) {
+      Ravel.room('/test/:testId/entity/:entityId');
+      expect(rooms['/test/:testId/entity/:entityId']).have.property('name').that.equals('/test/:testId/entity/:entityId');
+      expect(rooms['/test/:testId/entity/:entityId']).have.property('params').that.deep.equals(['testId', 'entityId']);
+      expect(rooms['/test/:testId/entity/:entityId']).have.property('regex').that.deep.equals(new RegExp('/test/(\\w+)/entity/(\\w+)'));
+      expect(rooms['/test/:testId/entity/:entityId']).have.property('authorize').that.is.a('function');
+      rooms['/test/:testId/entity/:entityId'].authorize(1, function(err, isAuthorized) {
+        expect(err).to.be.null;
+        expect(isAuthorized).to.be.ok;
+        done();
+      });
+    });
+
+    //TODO room path normalization
+  });
+});
