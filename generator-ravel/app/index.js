998366582c7ae0d8a9d21941bb8e94a9f0f9f8bd
Lyon Lay <llay@uncharted.software>
2015-06-11 16:17:23 -0400

    Added empty generator

---
+++ generator-ravel/app/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  foobar: function () {
+    console.log('Empty generator. Add functionality to generator-ravel/app/index.js');
+  }
+});
