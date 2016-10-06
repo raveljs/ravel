871fe3b0fc729224ac26edc0d8324de905459c42
Sean McIntyre <s.mcintyre@xverba.ca>
2016-10-06 14:55:25 -0400

    Non-ravel errors thrown in a routes/resource handler should have their message included in the response body.

--- test/util/test-rest.js
+++ test/util/test-rest.js
@@ -199,7 +199,7 @@ describe('util/rest', function() {
       });
       request(app.callback())
       .get('/')
-      .expect(500, 'Internal Server Error', done); // error message should not be exposed
+      .expect(500, 'a message', done); // error message should not be exposed
     });
   });
 });
