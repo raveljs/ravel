9b5e2f4f1626d64ac48c13f0e68b32326e048263
Sean McIntyre <s.mcintyre@xverba.ca>
2018-06-04 16:24:38 -0400

    Hooking up vscode to jest. Getting initial test working

--- jest/core/module.test.js
+++ jest/core/module.test.js
@@ -1,7 +1,14 @@
 describe('Ravel', () => {
   describe('@Module', () => {
     it('should do something', () => {
-      console.log('hello world');
+      @Ravel.Module('test')
+      class Test {
+        method () {
+          // do something
+        }
+      }
+      const app = new Ravel();
+      app.load(Test);
     });
   });
 });
--- jest/setup.js
+++ jest/setup.js
@@ -1,1 +1,4 @@
 global.mockery = require('mockery');
+global.sinon = require('sinon');
+global.upath = require('upath');
+global.Ravel = require('../lib/ravel');
