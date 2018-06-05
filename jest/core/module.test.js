b02866aeba4204f4b75b536c6a335e205c346b32
Sean McIntyre <s.mcintyre@xverba.ca>
2018-06-05 10:04:06 -0400

    Making modules more testable.

--- jest/core/module.test.js
+++ jest/core/module.test.js
@@ -1,14 +1,21 @@
 describe('Ravel', () => {
   describe('@Module', () => {
-    it('should do something', () => {
+    it('should register modules for instantiation and initialization in Ravel.init', async () => {
+      const spy = jest.fn();
       @Ravel.Module('test')
       class Test {
         method () {
-          // do something
+          spy();
         }
       }
       const app = new Ravel();
+      app.set('keygrip keys', ['abc']);
+      app.set('log level', app.log.NONE);
       app.load(Test);
+      await app.init();
+      expect(app.module('test')).toBeDefined();
+      app.module('test').method();
+      expect(spy).toHaveBeenCalled();
     });
   });
 });
