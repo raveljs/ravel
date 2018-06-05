dc2b3298322fa15b7eb3e90bb02df07be6afbbf9
Sean McIntyre <s.mcintyre@xverba.ca>
2018-06-05 11:46:29 -0400

    Figuring out jest mocking

--- jest/core/scan.test.js
+++ jest/core/scan.test.js
@@ -1,7 +1,35 @@
 describe('Ravel', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  // Testing how Ravel loads modules
   describe('scan', () => {
+    it('should attempt to load classes from files within a directory', () => {
+      jest.doMock('fs-readdir-recursive', () => {
+        return () => ['test1.js', 'test2.js', '.eslintrc', 'package/test3.js'];
+      });
+      const fs = require('fs');
+      jest.spyOn(fs, 'lstatSync').mockImplementation(function () {
+        return {
+          isDirectory: function () { return true; }
+        };
+      });
+      const app = new (require('../../lib/ravel'))();
+      const m1 = class {};
+      const m2 = class {};
+      const m3 = class {};
+      jest.doMock(upath.join(app.cwd, './modules/test1.js'), () => m1, {virtual: true});
+      jest.doMock(upath.join(app.cwd, './modules/test2.js'), () => m2, {virtual: true});
+      jest.doMock(upath.join(app.cwd, './modules/package/test3.js'), () => m3, {virtual: true});
+      app.load = jest.fn();
+      app.scan('./modules');
+      expect(app.load).toHaveBeenCalledWith(m1, m2, m3);
+    });
+
     describe('@Module', () => {
       it('should register modules for instantiation and initialization in Ravel.init', async () => {
+        const Ravel = require('../../lib/ravel');
         const spy = jest.fn();
         @Ravel.Module('test')
         class Test {
--- jest/setup.js
+++ jest/setup.js
@@ -1,4 +1,1 @@
-global.mockery = require('mockery');
-global.sinon = require('sinon');
 global.upath = require('upath');
-global.Ravel = require('../lib/ravel');
