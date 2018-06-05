19f001c66b689efbed09aebd441eb973ee64b3ed
Sean McIntyre <s.mcintyre@xverba.ca>
2018-06-05 13:43:03 -0400

    More tests

--- jest/core/scan.test.js
+++ jest/core/scan.test.js
@@ -56,6 +56,54 @@ describe('Ravel', () => {
         app.set('log level', app.log.NONE);
         expect(() => app.load(Test)).toThrowError(app.ApplicationError.IllegalValue);
       });
+
+      it('should throw a Ravel.ApplicationError.DuplicateEntry error when clients attempt to register multiple modules with the same name', () => {
+        const Ravel = require('../../lib/ravel');
+        @Ravel.Module('test')
+        class Test {}
+        @Ravel.Module('test')
+        class Test2 {}
+        const app = new Ravel();
+        app.set('log level', app.log.NONE);
+        expect(() => app.load(Test, Test2)).toThrowError(app.ApplicationError.DuplicateEntry);
+      });
+
+      it('should produce a module factory which can be used to instantiate the specified module and perform dependency injection of core services', async () => {
+        const Ravel = require('../../lib/ravel');
+        @Ravel.Module('test')
+        @Ravel.inject('$app', '$err', '$log', '$kvstore', '$params', '$db')
+        class Test {
+          constructor ($app, $err, $log, $kvstore, $params, $db) {
+            this.$app = $app;
+            this.$err = $err;
+            this.$log = $log;
+            this.$kvstore = $kvstore;
+            this.$params = $params;
+            this.$db = $db;
+          }
+        }
+        const app = new Ravel();
+        app.set('keygrip keys', ['abc']);
+        app.set('log level', app.log.NONE);
+        app.load(Test);
+        await app.init();
+        const instance = app.module('test');
+        expect(instance).toBeDefined();
+        expect(instance.$app).toEqual(app);
+        expect(instance.$err).toEqual(app.ApplicationError);
+        expect(instance.$log).toBeDefined();
+        expect(instance.$log).toHaveProperty('trace');
+        expect(instance.$log).toHaveProperty('verbose');
+        expect(instance.$log).toHaveProperty('debug');
+        expect(instance.$log).toHaveProperty('info');
+        expect(instance.$log).toHaveProperty('warn');
+        expect(instance.$log).toHaveProperty('error');
+        expect(instance.$log).toHaveProperty('critical');
+        expect(instance.$kvstore).toEqual(app.kvstore);
+        expect(instance.$params).toBeDefined();
+        expect(instance.$params).toHaveProperty('get');
+        expect(instance.$db).toHaveProperty('scoped');
+      });
     });
   });
 });
