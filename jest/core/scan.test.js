3458ce7fe51a21d08688131632d7fae666df4e8c
Sean McIntyre <s.mcintyre@xverba.ca>
2018-06-05 10:06:30 -0400

    Scoping tests differently

---
+++ jest/core/scan.test.js
@@ -0,0 +1,23 @@
+describe('Ravel', () => {
+  describe('scan', () => {
+    describe('@Module', () => {
+      it('should register modules for instantiation and initialization in Ravel.init', async () => {
+        const spy = jest.fn();
+        @Ravel.Module('test')
+        class Test {
+          method () {
+            spy();
+          }
+        }
+        const app = new Ravel();
+        app.set('keygrip keys', ['abc']);
+        app.set('log level', app.log.NONE);
+        app.load(Test);
+        await app.init();
+        expect(app.module('test')).toBeDefined();
+        app.module('test').method();
+        expect(spy).toHaveBeenCalled();
+      });
+    });
+  });
+});
