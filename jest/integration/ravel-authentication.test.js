e05f11b94a5caa30fb98c3b127e6d8bd595c19de
Sean McIntyre <s.mcintyre@xverba.ca>
2018-06-11 14:51:24 -0400

    Porting authentication integration tests

--- jest/integration/ravel-authentication.test.js
+++ jest/integration/ravel-authentication.test.js
@@ -1,51 +1,16 @@
-'use strict';
-
-const chai = require('chai');
-const expect = chai.expect;
-const sinon = require('sinon');
-chai.use(require('chai-as-promised'));
-chai.use(require('chai-things'));
-chai.use(require('sinon-chai'));
-const mockery = require('mockery');
-const upath = require('upath');
-const request = require('supertest');
-
-let Ravel, ravelApp, agent;
-
-const profile = {
-  id: 1234,
-  name: 'Sean McIntyre',
-  password: 'abcd'
-};
-
 describe('Authentication Integration Test', () => {
-  beforeEach((done) => {
-    // enable mockery
-    mockery.enable({
-      useCleanCache: true,
-      warnOnReplace: false,
-      warnOnUnregistered: false
-    });
+  let Ravel, app;
+  const profile = {
+    id: 1234,
+    name: 'Sean McIntyre',
+    password: 'abcd'
+  };
 
-    // mock ravelApp.kvstore, since we're not actually starting ravelApp.
-    mockery.registerMock('redis', require('redis-mock'));
+  beforeEach(async () => {
     Ravel = require('../../lib/ravel');
-    ravelApp = new Ravel();
-    ravelApp.set('log level', ravelApp.log.NONE);
-    ravelApp.log.setLevel('NONE');
-    ravelApp.set('redis host', 'localhost');
-    ravelApp.set('redis port', 5432);
-    ravelApp.set('port', '9080');
-    ravelApp.set('koa public directory', 'public');
-    ravelApp.set('keygrip keys', ['mysecret']);
-    done();
-  });
-
-  afterEach((done) => {
-    ravelApp = undefined;
-    mockery.deregisterAll();
-    mockery.disable();
-    done();
+    app = new Ravel();
+    app.set('log level', app.log.NONE);
+    app.set('keygrip keys', ['mysecret']);
   });
 
   describe('Simulated Local Auth Provider', () => {
@@ -104,7 +69,12 @@ describe('Authentication Integration Test', () => {
       }
 
       @Ravel.Module.authconfig
-      class AuthConfig extends Ravel.Module {
+      @Ravel.Module('authconfig')
+      @Ravel.inject('$err')
+      class AuthConfig {
+        constructor ($err) {
+          this.$err = $err;
+        }
         serializeUser (profile) {
           return Promise.resolve(profile.id);
         }
@@ -112,14 +82,14 @@ describe('Authentication Integration Test', () => {
           if (userId === profile.id) {
             return Promise.resolve(profile);
           } else {
-            return Promise.reject(new this.ApplicationError.Authentication('User session cannot be found.'));
+            return Promise.reject(new this.$err.Authentication('User session cannot be found.'));
           }
         }
         verify (provider, username, password) {
           if (username === profile.name && password === profile.password) {
             return Promise.resolve(profile);
           } else {
-            return Promise.reject(new this.ApplicationError.Authentication('Username or password is incorrect'));
+            return Promise.reject(new this.$err.Authentication('Username or password is incorrect'));
           }
         }
       }
@@ -128,12 +98,9 @@ describe('Authentication Integration Test', () => {
       const mapping = Ravel.Routes.mapping;
       const authenticated = Ravel.Routes.authenticated;
 
+      @Ravel.Routes('/')
       @mapping(Ravel.Routes.DELETE, '/app', Ravel.httpCodes.NOT_IMPLEMENTED)
-      class TestRoutes extends Ravel.Routes {
-        constructor () {
-          super('/');
-        }
-
+      class TestRoutes {
         @authenticated
         @mapping(Ravel.Routes.GET, '/app')
         async appHandler (ctx) {
@@ -149,85 +116,84 @@ describe('Authentication Integration Test', () => {
         }
       }
 
-      new LocalProvider(ravelApp); // eslint-disable-line no-new
-      mockery.registerMock(upath.join(ravelApp.cwd, 'authconfig'), AuthConfig);
-      ravelApp.module('authconfig', 'authconfig');
-      mockery.registerMock(upath.join(ravelApp.cwd, 'routes'), TestRoutes);
-      ravelApp.routes('routes');
-      await ravelApp.init();
-      agent = request.agent(ravelApp.server); //eslint-disable-line
+      new LocalProvider(app); // eslint-disable-line no-new
+      app.load(AuthConfig, TestRoutes);
+      await app.init();
     });
 
-    afterEach(() => {
-      return ravelApp.close();
+    afterEach(async () => {
+      await app.close();
     });
 
-    it('should support local auth on login route', (done) => {
-      agent
+    it('should support local auth on login route', async () => {
+      await request(app.callback)
         .post('/auth/local')
         .type('application/json')
         .send({ username: profile.name, password: profile.password })
-        .expect(200)
-        .end(done);
+        .expect(200);
     });
 
-    it('should reject incorrect passwords on login route', (done) => {
-      agent
+    it('should reject incorrect passwords on login route', async () => {
+      await request(app.callback)
         .post('/auth/local')
         .type('application/json')
         .send({ username: profile.name, password: 'wrongpassword' })
-        .expect(401)
-        .end(done);
+        .expect(401);
     });
 
-    it('should reject unknown users on login route', (done) => {
-      agent
+    it('should reject unknown users on login route', async () => {
+      await request(app.callback)
         .post('/auth/local')
         .type('application/json')
         .send({ username: 'wrongname', password: 'wrongpassword' })
-        .expect(401)
-        .end(done);
+        .expect(401);
     });
 
-    it('should reject users on an @authenticated route', (done) => {
-      agent
+    it('should reject users on an @authenticated route', async () => {
+      await request(app.callback)
         .get('/app')
-        .expect(401)
-        .end(done);
+        .expect(401);
     });
 
-    it('should allow access to authenticated users on @authenticated routes', (done) => {
-      agent
+    it('should allow access to authenticated users on @authenticated routes', async () => {
+      // TODO remove workaround.
+      // cookies are busted in jest due to bugs:
+      // https://github.com/visionmedia/supertest/issues/336
+      // https://github.com/facebook/jest/issues/3547
+      // https://github.com/visionmedia/supertest/issues/460
+      // https://github.com/facebook/jest/issues/2549
+      const agent = request.agent(app.server);
+      const res = await agent
         .post('/auth/local')
         .type('application/json')
         .send({ username: profile.name, password: profile.password })
-        .end((err) => {
-          if (err) done(err);
-          agent.get('/app')
-            .expect(200, '<!DOCTYPE html><html></html>')
-            .end(done);
-        });
+        .expect(200);
+      const cookies = res.headers['set-cookie'][0].split(',').map(item => item.split(';')[0]).join(';');
+      await agent
+        .get('/app')
+        .set('Cookie', cookies)
+        .expect(200, '<!DOCTYPE html><html></html>');
     });
 
-    it('should log a deprecation message for use of ctx.passport', (done) => {
-      const spy = sinon.spy(ravelApp.log, 'warn');
-      agent
+    it('should log a deprecation message for use of ctx.passport', async () => {
+      // TODO remove workaround.
+      // cookies are busted in jest due to bugs:
+      // https://github.com/visionmedia/supertest/issues/336
+      // https://github.com/facebook/jest/issues/3547
+      // https://github.com/visionmedia/supertest/issues/460
+      // https://github.com/facebook/jest/issues/2549
+      const spy = jest.spyOn(app.log, 'warn');
+      const agent = request.agent(app.server);
+      const res = await agent
         .post('/auth/local')
         .type('application/json')
-        .send({ username: profile.name, password: profile.password })
-        .end((err) => {
-          if (err) done(err);
-          agent.get('/deprecated')
-            .expect(200)
-            .end((err) => {
-              try {
-                expect(spy).to.have.been.calledWith('ctx.passport is deprecated. Please use ctx.state instead.');
-                done(err);
-              } catch (err2) {
-                done(err2);
-              }
-            });
-        });
+        .send({ username: profile.name, password: profile.password });
+      const cookies = res.headers['set-cookie'][0].split(',').map(item => item.split(';')[0]).join(';');
+      await agent
+        .get('/deprecated')
+        .set('Cookie', cookies)
+        .expect(200);
+      expect(spy).toHaveBeenCalledWith('ctx.passport is deprecated. Please use ctx.state instead.');
     });
   });
 });
