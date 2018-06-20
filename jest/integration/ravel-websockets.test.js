dd5f417fea751840925ce12c01ba02de9de40724
Sean McIntyre <s.mcintyre@xverba.ca>
2018-06-20 14:54:49 -0400

    Semi-functional implementation

--- jest/integration/ravel-websockets.test.js
+++ jest/integration/ravel-websockets.test.js
@@ -35,29 +35,66 @@ describe('Websocket Integration Test', () => {
   });
 
   describe('subscription', () => {
-    let ws, wsSession;
+    let ws, agent;
 
     beforeEach(async () => {
+      // test routes
+      @Ravel.Routes('/ws')
+      @Ravel.autoinject('$ws')
+      class WS {
+        @Ravel.Routes.mapping(Ravel.Routes.POST, 'subscribe')
+        async subHandler (ctx) {
+          ctx.body = await this.$ws.subscribe('a.channel', ctx);
+        }
+        @Ravel.Routes.mapping(Ravel.Routes.DELETE, 'subscribe')
+        async unsubHandler (ctx) {
+          ctx.body = await this.$ws.unsubscribe('a.channel', ctx);
+        }
+        @Ravel.Routes.mapping(Ravel.Routes.POST, 'publish')
+        async publishHandler (ctx) {
+          ctx.body = await this.$ws.publish('a.channel', 'message');
+        }
+      }
+      app.load(WS);
+      // bootstrap app
       await app.init();
       await app.listen();
       // establish client connection
-      const ws = new WebSocket('ws://0.0.0.0:8080');
-      let wsSession;
+      ws = new WebSocket('ws://0.0.0.0:8080');
+      let wsResponseCookies;
       await new Promise((resolve, reject) => {
         ws.on('upgrade', (response) => {
-          wsSession = response.headers['set-cookie'].join(';').match(/ravel.ws.id=(\w+)/)[1];
+          wsResponseCookies = response.headers['set-cookie'];
         });
         ws.on('open', resolve);
         ws.on('error', reject);
       });
+      agent = request.agent(app.callback);
+      wsResponseCookies[0]
+        .split(',')
+        .map(item => item.split(';')[0])
+        .forEach(c => agent.jar.setCookie(c));
     });
 
-    afterEach(async => {
+    afterEach(() => {
       ws.close();
     });
 
-    it('should', () => {
+    it('should ensure clients do not receive messages for topics they are not subscribed to', async () => {
+      const messageSpy = jest.fn();
+      ws.on('message', messageSpy);
+      await agent.post('/ws/publish').expect(201);
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      expect(messageSpy).not.toHaveBeenCalled();
+    });
 
+    it('should allow clients to subscribe to topics and receive messages', async () => {
+      const messageSpy = jest.fn();
+      ws.on('message', messageSpy);
+      await agent.post('/ws/subscribe').expect(201);
+      await agent.post('/ws/publish').expect(201);
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      expect(messageSpy).toHaveBeenCalled();
     });
   });
 });
